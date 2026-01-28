7feb1298ef9be09a3dccea696aed83ae28b57059
andres-m-rodriguez <andresm.rodriguezcan@gmail.com>
2026-01-28 13:31:10 -0400

    Add Wildlife Discovery database models and migrations
    Implements database layer for Wildlife Discovery sighting feature:
    - Sighting entity with all required fields (species, location, confidence, etc.)
    - UserSpecies entity for user-submitted species not in database
    - Supporting enums: SightingMode, ConfidenceLevel, CountRange, Behavior, EvidenceType, Weather, SightingStatus
    - Database migration for new tables with proper indexes and relationships
    - Student and Teacher roles added to UserRole enum
    
    Closes #118

--- src/FaunaFinder.Database/Migrations/20260127015033_AddWildlifeDiscoveryTables.Designer.cs
+++ src/FaunaFinder.Database/Migrations/20260127015033_AddWildlifeDiscoveryTables.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FaunaFinder.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NetTopologySuite.Geometries;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace FaunaFinder.Database.Migrations
 {
     [DbContext(typeof(FaunaFinderContext))]
-    partial class FaunaFinderContextModelSnapshot : ModelSnapshot
+    [Migration("20260127015033_AddWildlifeDiscoveryTables")]
+    partial class AddWildlifeDiscoveryTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/FaunaFinder.Database/Models/Sightings/CountRange.cs
@@ -0,0 +1,9 @@
+namespace FaunaFinder.Database.Models.Sightings;
+
+public enum CountRange
+{
+    One = 0,
+    TwoToFive = 1,
+    SixToTwenty = 2,
+    TwentyPlus = 3
+}
